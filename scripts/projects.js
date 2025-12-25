fa073eb3d88cb89b3a0be440a1c82cf4214e81db
Ababei Raul Costin <100480916+costitza@users.noreply.github.com>
2025-12-25 14:25:12 +0200

    getting projects from json

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,35 +1,41 @@
 
 
-// Option 1: Hardcode projects in JS
-const projects = [
-    {
-        title: "Quest",
-        description: "inovative robotics project made from scratch, trying to combine a drone with a rover",
-        link: "quest.html"
-    },
-    {
-        title: "Udraw",
-        description: "drawing application written in C# using Windows Forms and SQL database to store drawings",
-        link: "udraw.html"
-    },
-];
+let projects = [];
+
+// AJAX Request (Fetch JSON)
+fetch('../data/projects.json')
+    .then(response => response.json())
+    .then(data => {
+        projects = data;
+    })
+    .catch(err => console.error("Error loading projects:", err));
 
 // Select container
 const container = document.querySelector(".projects-container");
 
-// Generate cards
-projects.forEach(project => {
-    const card = document.createElement("div");
-    card.classList.add("project-box");
-    card.innerHTML = `
-        <h3>${project.title}</h3>
-        <p>${project.description}</p>
-    `;
+// Wait for projects to load before generating cards
+setTimeout(() => {
+    generateProjectCards();
+}, 500);
 
-    // Open detailed project page on click
-    card.addEventListener("click", () => {
-        window.location.href = project.link;
+function generateProjectCards() {
+
+
+    // Generate cards
+    projects.forEach(project => {
+        const card = document.createElement("div");
+        card.classList.add("project-box");
+        card.innerHTML = `
+            <h3>${project.title}</h3>
+            <h5>${project.category}</h5>
+            <p>${project.description}</p>
+        `;
+
+        // Open detailed project page on click
+        card.addEventListener("click", () => {
+            window.location.href = project.link;
+        });
+
+        container.appendChild(card);
     });
-
-    container.appendChild(card);
-});
+}
